929411396787a1bbdf34869770c5c7c5ee0d29dc
tsukaryu <drgon8otsuka@gmail.com>
2020-11-26 00:23:24 +0900

    Add modeling-genre

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,40 @@
+class GenresController < ApplicationController
+  def index
+    @genres = Genre.all
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to genres_url, notice: "「#{@genre.name}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def update
+    genre = Genre.find(params[:id])
+    genre.update!(genre_params)
+    redirect_to genres_url, notice: "「#{genre.name}」を更新しました。"
+  end
+
+  def destroy
+    genre = Genre.find(params[:id])
+    genre.destroy
+    redirect_to genres_url, notice: "「#{genre.name}」を削除しました。"
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+end
