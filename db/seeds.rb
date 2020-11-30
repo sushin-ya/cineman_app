9ad55b20d3a03610061c2d2f4b9c3f57f20d05df
tsukaryu <drgon8otsuka@gmail.com>
2020-11-30 21:37:56 +0900

    Add user delete function

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,8 @@
 User.create!(name: 'Test User',
              email: 'test-0@test.com',
              password: 'password',
-             password_confirmation: 'password')
+             password_confirmation: 'password',
+             admin: true)
 
 # 追加のユーザーをまとめて生成する
 99.times do |n|
