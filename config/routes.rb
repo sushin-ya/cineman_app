53d94920473607438e6678835e6c907cf27b90d6
tsukaryu <drgon8otsuka@gmail.com>
2020-12-08 05:09:45 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,5 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-
   delete '/logout', to: 'sessions#destroy'
 end
