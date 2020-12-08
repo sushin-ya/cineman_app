df9a86f6942a41cd7117cece2ba380675e363ed1
tsukaryu <drgon8otsuka@gmail.com>
2020-12-09 08:37:19 +0900

    Finish production_year function

--- app/controllers/production_years_controller.rb
+++ app/controllers/production_years_controller.rb
@@ -1,7 +1,11 @@
 class ProductionYearsController < ApplicationController
   def index
+    @production_years = [*1940..2020].select { |num| (num % 10).zero? }
   end
 
-  def movies    
+  def movies
+    start_year = params[:production_year_id]
+    end_year = start_year.to_i + 9
+    @movies = Movie.where(production_year: start_year.to_s..end_year.to_s).all
   end
 end
